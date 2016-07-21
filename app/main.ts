63519c781ffc39fe5a91b1a80610136652fe74f5
Neron X5 <x5.neron@gmail.com>
2016-07-21 13:02:37 +0530

    Seperate App component into seperate components

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,5 @@
+import { enableProdMode } from '@angular/core';
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app.component';
+enableProdMode();
 bootstrap(AppComponent);
